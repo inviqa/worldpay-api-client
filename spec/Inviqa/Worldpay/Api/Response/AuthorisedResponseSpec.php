19789f6c7c2355cfef0360146fc9c065c297edd7
Liviu Panainte <lpanainte@inviqa.com>
2018-03-07 11:24:05 +0000

    Process 3DS response

--- spec/Inviqa/Worldpay/Api/Response/AuthorisedResponseSpec.php
+++ spec/Inviqa/Worldpay/Api/Response/AuthorisedResponseSpec.php
@@ -10,6 +10,7 @@ use Prophecy\Argument;
 class AuthorisedResponseSpec extends ObjectBehavior
 {
     const XML = "<reply><orderStatus orderCode=\"order-ecomm-test-123\"><lastEvent>AUTHORISED</lastEvent></orderStatus></reply>";
+    const XML_3DS = "<reply><orderStatus orderCode=\"order-ecomm-test-123\"><request3DSecure><paRequest>abc</paRequest><issuerURL>localhost</issuerURL></request3DSecure></orderStatus></reply>";
     const ERROR_XML = "<reply><error code=\"" . self::ERROR_CODE . "\"><![CDATA[" . self::ERROR_MSG . "]]></error></reply>";
     const ERROR_MSG = "An internal CSE service error has occurred.";
     const ERROR_CODE = "5";
@@ -29,5 +30,28 @@ class AuthorisedResponseSpec extends ObjectBehavior
 
         $this->errorCode()->shouldReturn(self::ERROR_CODE);
         $this->errorMessage()->shouldReturn(self::ERROR_MSG);
+        $this->paRequestValue()->shouldReturn("");
+    }
+
+    function it_returns_true_when_the_response_contains_the_request3DSecure_node()
+    {
+        $this->beConstructedWith(self::XML_3DS);
+
+        $this->is3DSecure()->shouldReturn(true);
+        $this->isSuccessful()->shouldReturn(false);
+    }
+
+    function it_returns_the_parequest_value_when_the_xml_contains_one()
+    {
+        $this->beConstructedWith(self::XML_3DS);
+
+        $this->paRequestValue()->shouldReturn("abc");
+    }
+
+    function it_returns_the_issuer_url_when_the_xml_contains_one()
+    {
+        $this->beConstructedWith(self::XML_3DS);
+
+        $this->issuerURL()->shouldReturn("localhost");
     }
 }
