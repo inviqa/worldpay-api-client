43c3b2622b2124297d25b3ad497a4dec6b67ff05
Tim Webster <tim.webster@inviqa.com>
2018-03-28 12:40:30 +0100

    fix for reference parsing

--- src/Inviqa/Worldpay/Notification/Response/NotificationResponse.php
+++ src/Inviqa/Worldpay/Notification/Response/NotificationResponse.php
@@ -11,14 +11,20 @@ class NotificationResponse
      */
     private $rawNotification;
 
+    /**
+     * @var array
+     */
+    private $reference;
+
     private function __construct()
     {
     }
 
     public static function fromRawNotification(string $rawNotification = "")
     {
-        $instance = new NotificationResponse();
+        $instance                  = new NotificationResponse();
         $instance->rawNotification = $rawNotification;
+        $instance->reference       = self::unserialiseReference($rawNotification);
 
         return $instance;
     }
@@ -26,24 +32,6 @@ class NotificationResponse
     public function isSuccessful(): bool
     {
         return strlen($this->nodeValue("lastEvent")) > 0;
-    }
-
-    private function nodeValue(string $nodeName): string
-    {
-        if (preg_match("~$nodeName>([^<]+)</$nodeName~", $this->rawNotification, $matches)) {
-            return $matches[1];
-        }
-
-        return '';
-    }
-
-    private function nodeAttributeValue(string $nodeName, string $attributeName): string
-    {
-        if (preg_match("~<$nodeName.*$attributeName=['\"]([^'\"]*)['\"]/?>~", $this->rawNotification, $matches)) {
-            return $matches[1];
-        }
-
-        return '';
     }
 
     public function orderCode(): string
@@ -68,26 +56,49 @@ class NotificationResponse
 
     public function refundValue()
     {
-        $xml = simplexml_load_string($this->rawNotification);
+        $xml = simplexml_load_string(self::rawNotification());
 
-        return (int) $xml->notify->orderStatusEvent->journal->accountTx[0]->amount->attributes()['value'];
+        return (int)$xml->notify->orderStatusEvent->journal->accountTx[0]->amount->attributes()['value'];
     }
 
     public function returnNumber()
     {
-        $xml = simplexml_load_string($this->rawNotification);
-
-        $reference = unserialize($xml->notify->orderStatusEvent->journal->journalReference->attributes()['reference']);
-
-        return $reference['returnNumber'] ?: '';
+        return $this->reference['returnNumber'] ?? '';
     }
 
     public function notifyClient()
     {
-        $xml = simplexml_load_string($this->rawNotification);
+        return $this->reference['notifyClient'] ?? false;
+    }
 
-        $reference = unserialize($xml->notify->orderStatusEvent->journal->journalReference->attributes()['reference']);
+    private function nodeValue(string $nodeName): string
+    {
+        if (preg_match("~$nodeName>([^<]+)</$nodeName~", $this->rawNotification, $matches)) {
+            return $matches[1];
+        }
 
-        return $reference['notifyClient'] ?: false;
+        return '';
+    }
+
+    private function nodeAttributeValue(string $nodeName, string $attributeName): string
+    {
+        if (preg_match("~<$nodeName.*$attributeName=['\"]([^'\"]*)['\"]/?>~", $this->rawNotification, $matches)) {
+            return $matches[1];
+        }
+
+        return '';
+    }
+
+    private function unserialiseReference($rawNotification)
+    {
+        $xml = simplexml_load_string($rawNotification);
+        $ref = $xml->notify->orderStatusEvent->payment->reference;
+
+        $serialisedData = @unserialize($ref);
+        if ($serialisedData !== false || $ref === 'b:0;') {
+            return $serialisedData;
+        }
+
+        return $ref;
     }
 }
