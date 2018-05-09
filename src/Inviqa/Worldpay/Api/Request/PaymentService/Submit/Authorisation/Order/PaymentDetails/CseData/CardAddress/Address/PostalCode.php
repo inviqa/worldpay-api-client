c037f6df13757ff0b3d269b10dcd7798914b2937
Liviu Panainte <lpanainte@inviqa.com>
2018-05-09 10:48:44 +0100

    Add default postalcode value

--- src/Inviqa/Worldpay/Api/Request/PaymentService/Submit/Authorisation/Order/PaymentDetails/CseData/CardAddress/Address/PostalCode.php
+++ src/Inviqa/Worldpay/Api/Request/PaymentService/Submit/Authorisation/Order/PaymentDetails/CseData/CardAddress/Address/PostalCode.php
@@ -6,8 +6,14 @@ use Inviqa\Worldpay\Api\XmlNodeDefaults;
 
 class PostalCode extends XmlNodeDefaults
 {
+    const DEFAULT_VALUE = "000000";
+
     public function __construct(string $string = null)
     {
+        if (empty($string)) {
+            $string = self::DEFAULT_VALUE;
+        }
+
         $this->string = $string;
     }
 }
