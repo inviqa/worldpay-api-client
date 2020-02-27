614b75c068f04bb1a58b5fc5c85ff0f128885304
Tim Webster <tim.webster@inviqa.com>
2020-02-27 14:00:25 +0000

    Update apple pay authorisation reequest to send full XML

--- src/Inviqa/Worldpay/Api/Request/AuthorizeRequestFactoryApplePay.php
+++ src/Inviqa/Worldpay/Api/Request/AuthorizeRequestFactoryApplePay.php
@@ -5,6 +5,7 @@ namespace Inviqa\Worldpay\Api\Request;
 use Inviqa\Worldpay\Api\Exception\InvalidRequestParameterException;
 use Inviqa\Worldpay\Api\Request\PaymentService\MerchantCode;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit;
+use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\AuthorisationOrder;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\AuthorisationOrderApplePay;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL\Data;
@@ -12,29 +13,16 @@ use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Paymen
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL\Header\EphemeralPublicKey;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL\Header\PublicKeyHash;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL\Header\TransactionId;
-use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Amount;
-use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Amount\CurrencyCode;
-use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Amount\Exponent;
-use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Amount\Value;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Description;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\OrderCode;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL\Signature;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\ApplePaySSL\Version as ApplePayVersion;
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetailsApplePay;
-use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Shopper\ShopperEmailAddress;
-use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\ShopperBasic;
 use Inviqa\Worldpay\Api\Request\PaymentService\Version;
 
 class AuthorizeRequestFactoryApplePay implements RequestFactory
 {
     private $defaultApplePayParameters = [
-        'version' => "1.4",
-        'orderCode' => "",
-        'description' => "",
-        'currencyCode' => "",
-        'exponent' => "2",
-        'value' => "",
-        'data' => "",
         'signature' => "dummy signature",
         'applePayVersion' => "dummy version",
         'ephemeralPublicKey' => "dummy public key",
@@ -50,21 +38,25 @@ class AuthorizeRequestFactoryApplePay implements RequestFactory
      */
     public function buildFromRequestParameters(array $parameters): PaymentService
     {
+        if (!empty($parameters['shippingAddress'])) {
+            $parameters['shippingAddress'] += AuthorizeRequestFactory::DEFAULT_ADDRESS_PARAMETERS;
+        }
+        $parameters += AuthorizeRequestFactory::DEFAULT_PARAMETERS;
         $parameters += $this->defaultApplePayParameters;
+
+        $treeBuilder = new AuthoriseRequestTreeBuilder($parameters);
+
         $orderCode = new OrderCode($parameters['orderCode']);
         $description = new Description($parameters['description']);
-        $amount = new Amount(
-            new CurrencyCode($parameters['currencyCode']),
-            new Exponent($parameters['exponent']),
-            new Value($parameters['value'])
-        );
+        $amount = $treeBuilder->buildAmount($parameters);
+
         $ephemeralPublicKey = new EphemeralPublicKey($parameters['ephemeralPublicKey']);
         $publicKeyHash = new PublicKeyHash($parameters['publicKeyHash']);
-        $transactonId = new TransactionId($parameters['transactionId']);
+        $transactionId = new TransactionId($parameters['transactionId']);
         $header = new Header(
             $ephemeralPublicKey,
             $publicKeyHash,
-            $transactonId
+            $transactionId
         );
         $signature = new Signature($parameters['signature']);
         $version = new ApplePayVersion($parameters['applePayVersion']);
@@ -77,15 +69,19 @@ class AuthorizeRequestFactoryApplePay implements RequestFactory
         );
 
         $paymentDetails = new PaymentDetailsApplePay($applePaySSL);
-        $emailAddress = new ShopperEmailAddress($parameters['email']);
-        $shopper = new ShopperBasic($emailAddress);
-        $order = new AuthorisationOrderApplePay(
+        $shopper = $treeBuilder->buildShopper();
+        $hcgAdditionalData = $treeBuilder->buildHcgAdditionalData();
+
+        $order = new AuthorisationOrder(
             $orderCode,
             $description,
             $amount,
             $paymentDetails,
-            $shopper
+            $shopper,
+            $hcgAdditionalData
         );
+        $order = $treeBuilder->buildAdditionalOrderDetails($parameters, $order);
+
         $paymentService = new PaymentService(
             new Version($parameters['version']),
             new MerchantCode($parameters['merchantCode']),
--- src/Inviqa/Worldpay/Api/Request/PaymentService/Submit/Authorisation/Order/PaymentDetails.php
+++ src/Inviqa/Worldpay/Api/Request/PaymentService/Submit/Authorisation/Order/PaymentDetails.php
@@ -6,24 +6,8 @@ use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Paymen
 use Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\PaymentDetails\Session;
 use Inviqa\Worldpay\Api\XmlNodeDefaults;
 
-class PaymentDetails extends XmlNodeDefaults
+interface PaymentDetails
 {
-    private $cseData;
-    private $session;
-
-    public function __construct(
-        CseData $cseData,
-        Session $session
-    )
-    {
-        $this->cseData = $cseData;
-        $this->session = $session;
-    }
-
-    public function xmlChildren()
-    {
-        return [
-            $this->cseData, $this->session
-        ];
-    }
+    public function xmlChildren();
+    public function xmlLabel();
 }
