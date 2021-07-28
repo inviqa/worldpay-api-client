78f1bf840d4bb7c0715dfd488a7924f63ed3d159
timcwebster <tim.webster@inviqa.com>
2021-07-28 17:01:06 +0100

    Hardcode sending of full page challenge window size

---
+++ src/Inviqa/Worldpay/Api/Request/PaymentService/Submit/Authorisation/Order/Additional3DSData/ChallengeWindowSize.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Inviqa\Worldpay\Api\Request\PaymentService\Submit\Authorisation\Order\Additional3DSData;
+
+use Inviqa\Worldpay\Api\XmlAttributeDefaults;
+
+class ChallengeWindowSize extends XmlAttributeDefaults
+{
+}
